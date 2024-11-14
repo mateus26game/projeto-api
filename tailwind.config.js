1b2ed5c783ea9266f6352bfc0b5277d6ca92e385
mateus26guerra <mateus26guerra@gmail.com>
2024-11-14 15:52:37 -0300

    isso

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Inclua todos os arquivos HTML e TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
