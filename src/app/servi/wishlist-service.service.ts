0851cf5f93b25282f7cefcce8b3bfae946b434b3
mateus26guerra <mateus26guerra@gmail.com>
2024-12-02 21:02:36 -0300

    erro 0

--- src/app/servi/wishlist-service.service.ts
+++ src/app/servi/wishlist-service.service.ts
@@ -4,28 +4,41 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class WishlistService {
-
   private wishlistKey = 'wishlist';
 
+  private isLocalStorageAvailable(): boolean {
+    return typeof window !== 'undefined' && typeof window.localStorage !== 'undefined';
+  }
+
   getWishlist(): number[] {
-    const stored = localStorage.getItem(this.wishlistKey);
-    return stored ? JSON.parse(stored) : [];
+    if (this.isLocalStorageAvailable()) {
+      const stored = localStorage.getItem(this.wishlistKey);
+      return stored ? JSON.parse(stored) : [];
+    }
+    return []; // Retorna uma lista vazia se localStorage não estiver disponível
   }
 
   addToWishlist(gameId: number): void {
-    const wishlist = this.getWishlist();
-    if (!wishlist.includes(gameId)) {
-      wishlist.push(gameId);
-      localStorage.setItem(this.wishlistKey, JSON.stringify(wishlist));
+    if (this.isLocalStorageAvailable()) {
+      const wishlist = this.getWishlist();
+      if (!wishlist.includes(gameId)) {
+        wishlist.push(gameId);
+        localStorage.setItem(this.wishlistKey, JSON.stringify(wishlist));
+      }
     }
   }
 
   removeFromWishlist(gameId: number): void {
-    const wishlist = this.getWishlist().filter(id => id !== gameId);
-    localStorage.setItem(this.wishlistKey, JSON.stringify(wishlist));
+    if (this.isLocalStorageAvailable()) {
+      const wishlist = this.getWishlist().filter(id => id !== gameId);
+      localStorage.setItem(this.wishlistKey, JSON.stringify(wishlist));
+    }
   }
 
   isInWishlist(gameId: number): boolean {
-    return this.getWishlist().includes(gameId);
+    if (this.isLocalStorageAvailable()) {
+      return this.getWishlist().includes(gameId);
+    }
+    return false; // Retorna falso se localStorage não estiver disponível
   }
 }
